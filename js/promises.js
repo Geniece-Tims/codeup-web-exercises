46a665e88d1e7b4280ca298d9f351ee14c2e8e80
Geniece Tims <geniece.c.tims@gmail.com>
2023-06-07 16:50:04 -0500

    completed exercise for handling an API Promise

--- js/promises.js
+++ js/promises.js
@@ -1,10 +1,37 @@
 var promisesAccessToken = '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'
+var username = 'Geniece-Tims'
 
+function getLastCommitDate() {
+    const url = `https://api.github.com/users/${username}/repos`;
 
+    return fetch(url)
+        .then(response => response.json())
+        .then(repos => {
+            if (repos.length === 0) {
+            throw new Error('User has no repositories.');
+            }
 
+            const latestRepo = repos.reduce((prev, current) => {
+                if (!prev || new Date(current.updated_at) > new Date(prev.updated_at)) {
+                    return current;
+            }
+            return prev;
+        });
 
+        const lastCommitDate = latestRepo.updated_at;
 
+        return lastCommitDate;
+    });
+}
 
+getLastCommitDate('Geniece-Tims')
+    .then(lastCommitDate => {
+        console.log('Last commit date:', lastCommitDate);
+    })
+
+    .catch(error => {
+        console.error('Error:', error);
+    })
 
 
 
@@ -28,5 +55,5 @@ function wait(num) {
     wait(1000).then(() => console.log('You\'ll see this after 1 second'));
     wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 }
-
+console.log(wait());
 
