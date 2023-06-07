7d99dc21cfb1227a9d17de5b7271565447b8a9b6
Geniece Tims <geniece.c.tims@gmail.com>
2023-06-06 21:39:12 -0500

    completed class selectors exercise for jQuery

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,11 +1,3 @@
 "use strict";
-
-const notice = $('#stay-cool').html();
-alert(notice);
-
-// $('.codeup').css('background-color', 'yellow');
-//
-// var contents = $('h1').html();
-// alert(contents);
-// $('h1, p, li').css('background-color', 'yellow');
+let elements = $('.codeup, #codeup, [title="codeup"]').css('border', '1px solid #F00');
 
